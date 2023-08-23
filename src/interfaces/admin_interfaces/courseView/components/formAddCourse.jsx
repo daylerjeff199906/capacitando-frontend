54a4b924e43dc0d920b11e8a4232f7e870ab3480
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-08-23 07:49:01 -0500

    add section list users

--- src/interfaces/admin_interfaces/courseView/components/formAddCourse.jsx
+++ src/interfaces/admin_interfaces/courseView/components/formAddCourse.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { Box, Button, Stack, TextField, Typography } from "@mui/material";
+import { Box, Button, Grid, Stack, TextField, Typography } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const FormAddCourse = () => {
@@ -21,58 +21,64 @@ const FormAddCourse = () => {
 
   return (
     <>
-      <Box>
-        <Stack spacing={1}>
-          <Typography variant="body1">Título</Typography>
-          <TextField
-            size="small"
-            placeholder="Ingrese el título del curso ..."
-            fullWidth
-          />
-          <Typography variant="body1">Descripción corta</Typography>
-          <TextField
-            size="small"
-            placeholder="Esta sección aparecerá junto a la imagen de portada, redacte una breve descripción del curso resaltando lo más importante..."
-            fullWidth
-            multiline
-            rows={4}
-          />
-          <Typography variant="body1">Url de video de presentación</Typography>
-          <TextField
-            size="small"
-            placeholder="Ingrese el título del curso ..."
-            fullWidth
-          />
-          <Typography variant="body1">Subir imagen de portada</Typography>
-          <input
-            type="file"
-            accept="image/*"
-            style={{ display: "none" }}
-            id="fileInput"
-            onChange={handleFileChange}
-          />
-          <label htmlFor="fileInput">
-            <Button
-              variant="contained"
-              component="span"
+      <Grid container spacing={2}>
+        <Grid item xs={6}>
+          <Stack spacing={1}>
+            <Typography variant="body1">Título</Typography>
+            <TextField
               size="small"
-              color="success"
+              placeholder="Ingrese el título del curso ..."
+              fullWidth
+            />
+            <Typography variant="body1">Descripción corta</Typography>
+            <TextField
+              size="small"
+              placeholder="Esta sección aparecerá junto a la imagen de portada, redacte una breve descripción del curso resaltando lo más importante..."
+              fullWidth
+              multiline
+              rows={4}
+            />
+            <Typography variant="body1">
+              Url de video de presentación
+            </Typography>
+            <TextField
+              size="small"
+              placeholder="Ingrese el título del curso ..."
+              fullWidth
+            />
+            <Typography variant="body1">Subir imagen de portada</Typography>
+            <input
+              type="file"
+              accept="image/*"
+              style={{ display: "none" }}
+              id="fileInput"
+              onChange={handleFileChange}
+            />
+            <label htmlFor="fileInput">
+              <Button
+                variant="contained"
+                component="span"
+                size="small"
+                color="success"
+              >
+                Subir imagen
+              </Button>
+            </label>
+            <div
+              onDragOver={handleDragOver}
+              onDrop={handleDrop}
+              style={{
+                border: "1px dashed #ccc",
+                padding: "20px",
+                marginTop: "10px",
+              }}
             >
-              Subir imagen
-            </Button>
-          </label>
-          <div
-            onDragOver={handleDragOver}
-            onDrop={handleDrop}
-            style={{
-              border: "1px dashed #ccc",
-              padding: "20px",
-              marginTop: "10px",
-            }}
-          >
-            Arrastra y suelta la imagen aquí
-          </div>
-
+              Arrastra y suelta la imagen aquí
+            </div>
+          </Stack>
+        </Grid>
+        <Grid item xs={6}></Grid>
+        <Grid item xs={12}>
           <Box
             sx={{
               display: "flex",
@@ -83,12 +89,14 @@ const FormAddCourse = () => {
             <Stack direction="row" spacing={2}>
               <Button variant="contained">Guardar</Button>
               <Link to="/dashboard/courses" style={{ textDecoration: "none" }}>
-                <Button variant="contained">Cancelar</Button>
+                <Button variant="contained" color={"error"}>
+                  Cancelar
+                </Button>
               </Link>
             </Stack>
           </Box>
-        </Stack>
-      </Box>
+        </Grid>
+      </Grid>
     </>
   );
 };
--- src/interfaces/admin_interfaces/courseView/sections/addCourseSection.jsx
+++ src/interfaces/admin_interfaces/courseView/sections/addCourseSection.jsx
@@ -1,4 +1,4 @@
-import { Box, Grid, Typography } from "@mui/material";
+import { Box, Divider, Grid, Typography } from "@mui/material";
 import FormAddCourse from "../components/formAddCourse";
 const AddCourseSection = () => {
   return (
@@ -9,20 +9,13 @@ const AddCourseSection = () => {
         </Typography>
       </Box>
       <Grid container spacing={2} sx={{ paddingRight: 4 }}>
-        <Grid item xs={6}>
+        <Grid item xs={12}>
           <Box padding={3} backgroundColor="#ffffff" borderRadius={4}>
-            <Typography variant="h6" paddingY={3}>
+            <Typography variant="h6" paddingY={1}>
               Añadir datos de curso
             </Typography>
+            <Divider sx={{marginBottom:2}}/>
             <FormAddCourse />
-          </Box>
-        </Grid>
-        <Grid item xs={6}>
-          <Box padding={3} backgroundColor="#ffffff" borderRadius={4}>
-            <Typography variant="h6" paddingY={3}>
-              Detalles de curso
-            </Typography>
-            {/* <DetailsUserSection /> */}
           </Box>
         </Grid>
       </Grid>
