989b9e7ba1028455c80c92aedf7da553b6a6b95d
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-09-01 01:43:34 -0500

    fixed card

--- src/components/CardCourse.jsx
+++ src/components/CardCourse.jsx
@@ -5,41 +5,51 @@ import {
   CardMedia,
   CardContent,
   Typography,
+  Tooltip,
 } from "@mui/material";
 import { Link } from "react-router-dom";
 
 const CardCourse = ({ name, description, image, units, time, url }) => {
   return (
-    <Card sx={{ maxWidth: 345, borderRadius: 4 }}>
-      <CardActionArea LinkComponent={Link} to={url}>
-        <CardMedia component="img" image={image} alt="Course Image" />
-        <CardContent sx={{ height: "auto" }}>
-          <Typography
-            gutterBottom
-            variant="h5"
-            component="div"
-            fontWeight={700}
-            fontFamily={"poppins"}
-            sx={{
-              textOverflow: "ellipsis",
-              overflow: "hidden",
-              whiteSpace: "nowrap",
-            }}
-          >
-            {name}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            {description}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Units: {units}
-          </Typography>
-          <Typography variant="body2" color="text.secondary">
-            Time: {time}
-          </Typography>
-        </CardContent>
-      </CardActionArea>
-    </Card>
+    <>
+      <Tooltip title={name} followCursor>
+        <Card sx={{ maxWidth: 345, borderRadius: 4 }}>
+          <CardActionArea LinkComponent={Link} to={url}>
+            <CardMedia component="img" image={image} alt="Course Image" />
+            <CardContent sx={{ height: "auto" }}>
+              <Typography
+                gutterBottom
+                variant="h5"
+                component="div"
+                fontWeight={700}
+                fontFamily={"poppins"}
+                sx={{
+                  textOverflow: "ellipsis",
+                  overflow: "hidden",
+                  whiteSpace: "nowrap",
+                }}
+              >
+                {name}
+              </Typography>
+              <Typography
+                variant="body2"
+                color="text.secondary"
+                fontWeight={700}
+                fontFamily={"poppins"}
+              >
+                {description}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Unidades: {units}
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                Total de horas: {time}
+              </Typography>
+            </CardContent>
+          </CardActionArea>
+        </Card>
+      </Tooltip>
+    </>
   );
 };
 
