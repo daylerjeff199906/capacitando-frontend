62d9a32cb95c58aa85afb359db606c0d4a51fc82
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-09-01 00:51:19 -0500

    fixed card course

--- src/components/CardCourse.jsx
+++ src/components/CardCourse.jsx
@@ -6,19 +6,21 @@ import {
   CardContent,
   Typography,
 } from "@mui/material";
+import { Link } from "react-router-dom";
 
-const CardCourse = ({ name, description, image, units, time }) => {
+const CardCourse = ({ name, description, image, units, time, url }) => {
   return (
     <Card sx={{ maxWidth: 345, borderRadius: 4 }}>
-      <CardActionArea>
-        <CardMedia
-          component="img"
-          height="140"
-          image={image}
-          alt="Course Image"
-        />
-        <CardContent>
-          <Typography gutterBottom variant="h5" component="div">
+      <CardActionArea LinkComponent={Link} to={url}>
+        <CardMedia component="img" image={image} alt="Course Image" />
+        <CardContent sx={{ height: "auto" }}>
+          <Typography
+            gutterBottom
+            variant="h5"
+            component="div"
+            fontWeight={700}
+            fontFamily={"poppins"}
+          >
             {name}
           </Typography>
           <Typography variant="body2" color="text.secondary">
