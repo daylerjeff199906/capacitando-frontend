4fb96e0ba50fc84b94513003062aed931f5cabf3
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-09-01 12:50:47 -0500

    fixed dashboard

--- src/layouts/protectedRoute.jsx
+++ src/layouts/protectedRoute.jsx
@@ -3,7 +3,7 @@ import { Outlet, Navigate } from "react-router-dom";
 import useAuth from "../hooks/useAuth";
 import Navbar from "../components/NavBar";
 import SideBar from "../components/SideBar";
-import { Grid } from "@mui/material";
+import { Box, Drawer, Grid } from "@mui/material";
 
 const ProtectedRoute = () => {
   const { auth, loading } = useAuth();
@@ -12,18 +12,28 @@ const ProtectedRoute = () => {
     return "Cargando...";
   }
 
-  
   return (
     <>
-      <Navbar />
+      <Box sx={{ display: "flex" }}>
+        <Navbar />
+        <Drawer
+        variant="permanent"
+        >
+          <SideBar />
+        </Drawer>
+        <Box component="main" sx={{ flexGrow: 1, p: 3 }}>
+          {auth?.idusuario ? <Outlet /> : <Navigate to="/" />}
+        </Box>
+      </Box>
+      {/* <Navbar />
       <Grid container spacing={4} sx={{ paddingTop: 8 }}>
         <Grid item xs={2}>
-          <SideBar />
+          
         </Grid>
         <Grid item xs={10}>
-          {auth?.idusuario ? <Outlet /> : <Navigate to="/" />}
+        
         </Grid>
-      </Grid>
+      </Grid> */}
     </>
   );
 };
