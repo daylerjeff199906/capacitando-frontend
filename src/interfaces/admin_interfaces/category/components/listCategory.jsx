96ad49d70d51171af8dc99d54dabc691227ece39
jpanaifo-dev <jose.santos@unapiquitos.edu.pe>
2023-09-03 09:48:23 -0500

    fixed add  course, course proveider

--- src/interfaces/admin_interfaces/category/components/listCategory.jsx
+++ src/interfaces/admin_interfaces/category/components/listCategory.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable react-hooks/exhaustive-deps */
 /* eslint-disable no-unused-vars */
 import React from "react";
 import {
@@ -27,7 +28,12 @@ const TableListCategorys = () => {
   const [page, setPage] = React.useState(0);
   const [rowsPerPage, setRowsPerPage] = React.useState(10);
 
-  const { categorys, editCategory, editStateCategory } = useCategory();
+  const { getCategory, categorys, editCategory, editStateCategory } =
+    useCategory();
+
+  React.useEffect(() => {
+    getCategory();
+  }, []);
 
   const getEstadoLabel = (estadoId) => {
     if (estadoId === 1) {
